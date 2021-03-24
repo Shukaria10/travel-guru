f22a0f26e3852ecea9c1d4339cc867e087d2d6dd
Kazi Samha Shukaria <67535910+Shukaria10@users.noreply.github.com>
2021-03-25 00:40:51 +0600

    Add files via upload

---
+++ build/precache-manifest.903ddaffd10def639a0b507fb9b940b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07be8ce609210145178502f4cd10625c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79196783f86e24b93dc1",
+    "url": "/static/css/main.4ed67824.chunk.css"
+  },
+  {
+    "revision": "8bcf367a68e811a1dd08",
+    "url": "/static/js/2.fee5c1d1.chunk.js"
+  },
+  {
+    "revision": "25384b8075375ad0074933b8ff38e12c",
+    "url": "/static/js/2.fee5c1d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79196783f86e24b93dc1",
+    "url": "/static/js/main.02998c9e.chunk.js"
+  },
+  {
+    "revision": "548616e762ee544bf5a8",
+    "url": "/static/js/runtime-main.42543a3e.js"
+  },
+  {
+    "revision": "32162f64d326b339d4a75b6d64d9e6c7",
+    "url": "/static/media/Rectangle 1.32162f64.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
